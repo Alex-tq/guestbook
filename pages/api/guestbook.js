a4d2de7692820b3d011c810fad8cbe6a8dd97eeb
unknown <alextq3036@gmail.com>
2021-11-21 21:51:42 -0500

    Added a placed the API logic inside a try catch block to handle erros

--- pages/api/guestbook.js
+++ pages/api/guestbook.js
@@ -1,16 +1,21 @@
 import {guests} from "../../data/guests"
 
 export default function handler (req, res){
-    if(req.method === 'GET'){
-        res.status(200).json(guests)
-    } else if (req.method === "POST"){
-        const guest = req.body
-        const newGuest = {
-            name: guest.name,
-            message: guest.message,
-            id: Date.now()
+    try{
+        if(req.method === 'GET'){
+            res.status(200).json(guests)
+        } else if (req.method === "POST"){
+            const guest = req.body
+            const newGuest = {
+                name: guest.name,
+                message: guest.message,
+                id: Date.now()
+            }
+            guests.push(newGuest)
+            res.status(201).json(newGuest)
         }
-        guests.push(newGuest)
-        res.status(201).json(newGuest)
+
+    }catch(e){
+        console.error(e)
     }
 }
