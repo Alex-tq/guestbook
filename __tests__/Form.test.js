510ec154787aa1ab53d3f3985c88b2b07d11ae26
unknown <alextq3036@gmail.com>
2021-12-15 23:55:00 -0500

    Added Tests to check in imput boxes changes when typed into

--- __tests__/Form.test.js
+++ __tests__/Form.test.js
@@ -3,7 +3,7 @@
  */
 
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Form from '../components/Form'
 
 test('The form renders correctly', ()=>{
@@ -20,4 +20,22 @@ test('The form renders correctly', ()=>{
     expect(textarea).toBeInTheDocument()
     expect(submitButton).toBeInTheDocument()
 
+})
+
+describe("Form", () => {
+    it('should be able to type into name input', async () => {
+        render(<Form />)
+
+        const nameInput = screen.getByPlaceholderText('Name')
+        fireEvent.change(nameInput, { target: {value: 'John'} })
+        expect(nameInput.value).toBe('John')
+    });
+
+    it('should be able to type into message input', async () => {
+        render(<Form />)
+
+        const textarea = screen.getByPlaceholderText('Message')
+        fireEvent.change(textarea, { target: {value: 'Hello'} })
+        expect(textarea.value).toBe('Hello')
+    });
 })
